704aca1a81b5988895e5db8653be41bcfc2b6ec3
Agustín de Prada <agustindeprada@outlook.com>
2025-08-15 11:32:20 -0300

    actualiza los archivos

--- app.js
+++ app.js
@@ -1,2 +1,21 @@
 // El principal objetivo de este desafío es fortalecer tus habilidades en lógica de programación. Aquí deberás desarrollar la lógica para resolver el problema.
 
+let listaAmigos = [];
+let inputAmigo = document.getElementById("amigo");
+let ulListaAmigos = document.getElementById("listaAmigos");
+
+console.log(inputAmigo)
+
+function agregarAmigos() {
+    listaAmigos.push(inputAmigo.value);
+}
+
+if (nombre === "") {
+    alert('Por favor, inserte un nombre.');
+    return;
+}
+
+
+function limpiarCampoDeEntrada() {
+    document.querySelector(listaAmigos).value = '';
+}
